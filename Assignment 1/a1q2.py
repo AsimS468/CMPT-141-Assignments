d9e38438962ed5d5b3ac18bde6f2a997022eb174
AsimS468 <144407671+AsimS468@users.noreply.github.com>
2024-08-20 17:48:09 -0400

    Add files via upload

---
+++ Assignment 1/a1q2.py
@@ -0,0 +1,11 @@
+print("Input an adverb:")
+blankWord1 = input()
+
+print("Input an adverb:")
+blankWord2 = input()
+
+print("Input pastense verb:")
+blankWord3 = input()
+
+print('Andrew ' + blankWord1 + ' told Sam that Zoe had ' + blankWord2 + ' ' + blankWord3 + ' to Brazil.')
+
